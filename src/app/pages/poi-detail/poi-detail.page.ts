44db362174ff53e58b80d42d490f6991b96b9547
Tiago Felipe Zardin <tiago.zardin@safeweb.com.br>
2025-09-28 18:57:30 -0300

    ajuste dos botões do gps

--- src/app/pages/poi-detail/poi-detail.page.ts
+++ src/app/pages/poi-detail/poi-detail.page.ts
@@ -3,7 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Poi } from '../../core/models';
 import { PoiService } from '../../services/poi.service';
 import { MapsService } from '../../services/maps.service';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonList, IonItem, IonLabel, IonButton, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonImg,
+  IonList, IonItem, IonLabel, IonButton, IonButtons, IonBackButton, IonIcon
+} from '@ionic/angular/standalone';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -12,27 +15,29 @@ import { NgIf } from '@angular/common';
   templateUrl: './poi-detail.page.html',
   imports: [
     IonHeader, IonToolbar, IonTitle, IonContent, IonImg,
-    IonList, IonItem, IonLabel, IonButton, IonButtons, IonBackButton, NgIf
+    IonList, IonItem, IonLabel, IonButton, IonButtons, IonBackButton, IonIcon,
+    NgIf
   ]
 })
 export class PoiDetailPage implements OnInit {
   poi?: Poi;
   defaultHref = '/regions';
-  constructor(private route: ActivatedRoute, private poiService: PoiService, private maps: MapsService) { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private poiService: PoiService,
+    private maps: MapsService
+  ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id')!;
     this.poiService.getById(id).subscribe(p => {
       this.poi = p;
-      // quando souber a região do POI, define o fallback do back
       if (p) this.defaultHref = `/region/${p.region}`;
     });
   }
+
   openGoogle() { if (this.poi) this.maps.openGoogleMaps(this.poi.lat, this.poi.lng); }
-  openWaze() { if (this.poi) this.maps.openWaze(this.poi.lat, this.poi.lng); }
-  openSite() {
-    if (this.poi?.website) {
-      window.open(this.poi.website, '_blank');
-    }
-  }
+  openWaze()   { if (this.poi) this.maps.openWaze(this.poi.lat, this.poi.lng); }
+  openSite()   { if (this.poi?.website) window.open(this.poi.website, '_blank'); }
 }
