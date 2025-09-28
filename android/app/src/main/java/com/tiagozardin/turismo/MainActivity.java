683601a5d0b93d5fd3842e352b3ffc59894324c0
Tiago Felipe Zardin <tiago.zardin@safeweb.com.br>
2025-09-28 20:22:27 -0300

    feat(android): add Capacitor Android platform, Gradle & app resources (icons/splash)

---
+++ android/app/src/main/java/com/tiagozardin/turismo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tiagozardin.turismo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tiagozardin.turismo',
+  appName: 'Turismo',
+  webDir: 'www'
+};
+
+export default config;
